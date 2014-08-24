64d6b009a1eb826df0a4153c6240d3708ec22e12
Ashima Athri <ashimaathri@gmail.com>
2014-08-24 11:12:56 -0400

    Introducing the feature layer class

---
+++ js/feature_layer.js
@@ -0,0 +1,83 @@
+define([
+    'dojo/_base/declare',
+    'esri/tasks/IdentifyTask',
+    'esri/layers/ArcGISDynamicMapServiceLayer'
+], function (declare, IdentifyTask, ArcGISDynamicMapServiceLayer) {
+  return declare([], {
+    constructor : function (opts) {
+      this._copyParameters(opts);
+      if(this.numIdentifyLayers()) {
+        this.createIdentifyTask();
+      }
+    },
+    
+    _copyParameters : function (opts) {
+      for(var key in opts) {
+        if(opts.hasOwnProperty(key)) {
+          this[key] = opts[key];
+        }
+      }
+    },
+
+    createIdentifyTask : function () {
+      this.task = new esri.tasks.IdentifyTask(this.getUrl());
+    },
+
+    getIdentifyTask : function () {
+      return this.task;
+    },
+
+    numIdentifyLayers : function () {
+      return this.identifyLayers.length;
+    },
+
+    getLabel : function () {
+      return this.label;
+    },
+
+    getUrl : function () {
+      return this.url;
+    },
+
+    isVisible : function () {
+      return this.visible;
+    },
+
+    getOpacity : function () {
+      return this.opacity;
+    },
+
+    getDynamicMapServiceLayer : function () {
+      return this.layer;
+    },
+
+    load : function () {
+      this.layer = new ArcGISDynamicMapServiceLayer(this.getUrl(),
+          this.getJSON());
+      return this.layer;
+    },
+
+    getIdentifyLayers : function () {
+      return this.identifyLayers;
+    },
+
+    getJSON : function () {
+      return {
+        label : this.getLabel(),
+        url   : this.getUrl(),
+        visible : this.isVisible(),
+        opacity : this.getOpacity(),
+        layer   : this.getDynamicMapServiceLayer(),
+        identifyLayers : this.getIdentifyLayers()
+      };
+    },
+
+    getInfo : function () {
+      return {
+        layer : this.getDynamicMapServiceLayer(),
+        title : this.getLabel(),
+        noLayers : true
+      };
+    }
+  });
+}); 
